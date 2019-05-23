e92caf119ea52f0699a0ba9bc73bfa0e2e0560ba
yuanyuxuan <37758881+yuanyuxuan@users.noreply.github.com>
2019-05-23 22:37:32 +0800

    Create testFunc.cpp

---
+++ testFunc.cpp
@@ -0,0 +1,13 @@
+#include "testFunc.h"
+
+int printHello()
+{
+  std::cout << "Hello world" << std::endl;
+  return 0;
+}
+
+int printfString(char *pStr)
+{
+  std::cout << pStr << std::endl;
+  return 0;
+}
